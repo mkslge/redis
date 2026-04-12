081304d482aa633095ca72acf5a80f9ceadd8aea
mkslge <marklseeliger@gmail.com>
2026-04-12 09:38:17 -0400

    removed cmake build files

--- Client/build/CMakeFiles/redisclient.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for redisclient.
-# This may be replaced when dependencies are built.
--- Client/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/markseeliger/Desktop/Coding/redisimpl/Client
-# Build directory: /Users/markseeliger/Desktop/Coding/redisimpl/Client/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/markseeliger/Desktop/Coding/redisimpl/Client/build/testClient[1]_include.cmake")
-subdirs("_deps/googletest-build")
